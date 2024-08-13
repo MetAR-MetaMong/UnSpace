9a539fb0febc101f0b5ff9fd9726ba4410a8bf29
WoojinKim1225 <kwj69641@gmail.com>
2024-08-13 21:17:40 +0900

    !BREAKINGCHANGE: Update Version to Unity6

--- Assets/Materials/Woojin/Volume/FakeVolumeCubeInclude.hlsl
+++ Assets/Materials/Woojin/Volume/FakeVolumeCubeInclude.hlsl
@@ -1,30 +1,30 @@
 #ifndef FAKEVOLUMECUBEINCLUDE
 #define FAKEVOLUMECUBEINCLUDE
 
-void CubeIntersect_float(float3 ro, float3 rd, out bool hasIntersection, out float3 posBehindOS) {
+void CubeIntersect_float(float3 ro, float3 rd, float cameraNearPlane, out bool isInVolume, out float3 posFarOS) {
     // Initialize t to a large number for comparison
     float t[3] = {FLT_MAX, FLT_MAX, FLT_MAX};
-    hasIntersection = false;
-
+    float ta[3] = {-FLT_MAX, -FLT_MAX, -FLT_MAX};
     // Check intersection with planes
     for (int i = 0; i < 3; i++) {
         if (rd[i] != 0) {
             float t1 = (0.5 - ro[i]) / rd[i];
             float t2 = (-0.5 - ro[i]) / rd[i];
-            if (t1 > 0 || t2 > 0) {
-                hasIntersection = true;
-                t[i] = max(t1, t2);
+            if (t1 > t2) {
+                t[i] = t1;
+                ta[i] = t2;
+            } else {
+                t[i] = t2;
+                ta[i] = t1;
             }
         }
     }
 
-    if (!hasIntersection) {
-        posBehindOS = ro; // Default value if no intersection
-        return;
-    }
+    float tFront = min(t[0], min(t[1], t[2]));
+    float tBehind = max(ta[0], max(ta[1], ta[2]));
+    posFarOS = ro + rd * tFront;
+    isInVolume = sign(tFront) * sign(tBehind-cameraNearPlane) < 0;
 
-    float tBehind = min(t[0], min(t[1], t[2]));
-    posBehindOS = ro + rd * tBehind;
 }
 
 #endif
